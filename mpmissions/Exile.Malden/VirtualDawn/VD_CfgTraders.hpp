f858eec1468075f593f94cbe764c216bc8a26d11
Sean A. Ragan <igaveupcrackforthis@gmail.com>
2019-01-06 13:30:41 -0500

    Create VD_CfgTraders.hpp
    Exile Malden mission custom Traders configuration file

---
+++ mpmissions/Exile.Malden/VirtualDawn/VD_CfgTraders.hpp
@@ -0,0 +1,147 @@
+/**
+ * Sells cars and general vehicles
+ */
+	class Exile_Trader_Vehicle
+	{
+		name				= "VEHICLE";
+		showWeaponFilter	= 0;
+		categories[]		=
+		{
+			"AAF_Anti_Air",					// VD_AllTraders.hpp
+			"CSAT_Anti_Air",				// VD_AllTraders.hpp
+			"CSAT_Pacific_Anti_Air",		// VD_AllTraders.hpp
+			"NATO_Anti_Air",				// VD_AllTraders.hpp
+			"NATO_Pacific_Anti_Air",		// VD_AllTraders.hpp
+
+			"AAF_APCs",						// VD_AllTraders.hpp
+			"CSAT_APCs",					// VD_AllTraders.hpp
+			"CSAT_Pacific_APCs",			// VD_AllTraders.hpp
+			"Exile_APCs",					// VD_AllTraders.hpp
+			"NATO_APCs",					// VD_AllTraders.hpp
+			"NATO_Pacific_APCs",			// VD_AllTraders.hpp
+
+			"AAF_Artillery",				// VD_AllTraders.hpp
+			"CSAT_Artillery",				// VD_AllTraders.hpp
+			"CSAT_Pacific_Artillery",		// VD_AllTraders.hpp
+			"NATO_Artillery",				// VD_AllTraders.hpp
+			"NATO_Pacific_Artillery",		// VD_AllTraders.hpp
+
+			"Civilian_Bicycles",			// VD_AllTraders.hpp
+
+			"AAF_Cars",						// VD_AllTraders.hpp
+			"Civilian_Cars",				// VD_AllTraders.hpp
+			"CSAT_Cars",					// VD_AllTraders.hpp
+			"CSAT_Pacific_Cars",			// VD_AllTraders.hpp
+			"CTRG_Cars",					// VD_AllTraders.hpp
+			"Exile_Cars",					// VD_AllTraders.hpp
+			"FIA_East_Cars",				// VD_AllTraders.hpp
+			"FIA_Independent_Cars",			// VD_AllTraders.hpp
+			"FIA_West_Cars",				// VD_AllTraders.hpp
+			"GEN_Cars",						// VD_AllTraders.hpp
+			"IDAP_Cars",					// VD_AllTraders.hpp
+			"NATO_Cars",					// VD_AllTraders.hpp
+			"NATO_Pacific_Cars",			// VD_AllTraders.hpp
+			"Syndikat_Cars",				// VD_AllTraders.hpp
+
+			"Civilian_Go_Carts",			// VD_AllTraders.hpp
+			"Vrana_Go_Carts",				// VD_AllTraders.hpp
+
+			"AAF_Motorcycles",				// VD_AllTraders.hpp
+			"Civilian_Motorcycles",			// VD_AllTraders.hpp
+			"CSAT_Motorcycles",				// VD_AllTraders.hpp
+			"CSAT_Pacific_Motorcycles",		// VD_AllTraders.hpp
+			"Exile_Motorcycles",			// VD_AllTraders.hpp
+			"FIA_East_Motorcycles",			// VD_AllTraders.hpp
+			"FIA_Independent_Motorcycles",	// VD_AllTraders.hpp
+			"FIA_West_Motorcycles",			// VD_AllTraders.hpp
+			"NATO_Motorcycles",				// VD_AllTraders.hpp
+			"NATO_Pacific_Motorcycles",		// VD_AllTraders.hpp
+
+			"AAF_Tanks",					// VD_AllTraders.hpp
+			"CSAT_Tanks",					// VD_AllTraders.hpp
+			"CSAT_Pacific_Tanks",			// VD_AllTraders.hpp
+			"NATO_Tanks",					// VD_AllTraders.hpp
+			"NATO_Pacific_Tanks",			// VD_AllTraders.hpp
+
+			"AAF_Trucks",					// VD_AllTraders.hpp
+			"CDF_Trucks",					// VD_AllTraders.hpp
+			"Civilian_Trucks",				// VD_AllTraders.hpp
+			"CSAT_Trucks",					// VD_AllTraders.hpp
+			"CSAT_Pacific_Trucks",			// VD_AllTraders.hpp
+			"Exile_Trucks",					// VD_AllTraders.hpp
+			"IDAP_Trucks",					// VD_AllTraders.hpp
+			"NATO_Trucks",					// VD_AllTraders.hpp
+			"NATO_Pacific_Trucks",			// VD_AllTraders.hpp
+
+			"AAF_Drones_Land",				// VD_AllTraders.hpp
+			"CSAT_Drones_Land",				// VD_AllTraders.hpp
+			"CSAT_Pacific_Drones_Land",		// VD_AllTraders.hpp
+			"NATO_Drones_Land"				// VD_AllTraders.hpp
+		};
+	}; // end class Exile_Trader_Vehicle
+
+/**
+ * Sells choppers and planes
+ */
+	class Exile_Trader_Aircraft
+	{
+		name				= "AIRCRAFT";
+		showWeaponFilter	= 0;
+		categories[]		=
+		{
+			"AAF_Planes",					// VD_AllTraders.hpp
+			"Civilian_Planes",				// VD_AllTraders.hpp
+			"CSAT_Planes",					// VD_AllTraders.hpp
+			"CSAT_Pacific_Planes",			// VD_AllTraders.hpp
+			"Exile_Planes",					// VD_AllTraders.hpp
+			"NATO_Planes",					// VD_AllTraders.hpp
+			"NATO_Pacific_Planes",			// VD_AllTraders.hpp
+			"Syndikat_Planes",				// VD_AllTraders.hpp
+
+			"AAF_Helicopters",				// VD_AllTraders.hpp
+			"Civilian_Helicopters",			// VD_AllTraders.hpp
+			"CSAT_Helicopters",				// VD_AllTraders.hpp
+			"CTRG_Helicopters",				// VD_AllTraders.hpp
+			"Exile_Helicopters",			// VD_AllTraders.hpp
+			"FIA_Independent_Helicopters",	// VD_AllTraders.hpp
+			"ION_Helicopters",				// VD_AllTraders.hpp
+			"NATO_Helicopters",				// VD_AllTraders.hpp
+			"Syndikat_Helicopters",			// VD_AllTraders.hpp
+			"Vrana_Helicopters",			// VD_AllTraders.hpp
+
+			"AAF_Drones_Aerial",			// VD_AllTraders.hpp
+			"Civilian_Drones_Aerial",		// VD_AllTraders.hpp
+			"CSAT_Drones_Aerial",			// VD_AllTraders.hpp
+			"CSAT_Pacific_Drones_Aerial",	// VD_AllTraders.hpp
+			"NATO_Drones_Aerial",			// VD_AllTraders.hpp
+			"NATO_Pacific_Drones_Aerial"	// VD_AllTraders.hpp
+		};
+	}; // end class Exile_Trader_Aircraft
+
+/**
+ * Sells ships and boats
+ */ 
+	class Exile_Trader_Boat
+	{
+		name				= "BOAT";
+		showWeaponFilter	= 0;
+		categories[]		=
+		{
+			"AAF_Boats",					// VD_AllTraders.hpp
+			"Civilian_Boats",				// VD_AllTraders.hpp
+			"CSAT_Boats",					// VD_AllTraders.hpp
+			"CSAT_Pacific_Boats",			// VD_AllTraders.hpp
+			"Exile_Boats",					// VD_AllTraders.hpp
+			"FIA_East_Boats",				// VD_AllTraders.hpp
+			"FIA_Independent_Boats",		// VD_AllTraders.hpp
+			"FIA_West_Boats",				// VD_AllTraders.hpp
+			"NATO_Boats",					// VD_AllTraders.hpp
+			"NATO_Pacific_Boats",			// VD_AllTraders.hpp
+			"Syndikat_Boats",				// VD_AllTraders.hpp
+
+			"AAF_Submersibles",				// VD_AllTraders.hpp
+			"CSAT_Submersibles",			// VD_AllTraders.hpp
+			"Exile_Submersibles",			// VD_AllTraders.hpp
+			"NATO_Submersibles"				// VD_AllTraders.hpp
+		};
+	}; // end class Exile_Trader_Boat
